bc9d89c106dad12c5506056c292dd38d8e32fbcf
pdhoward <patrick.howard@hotmail.com>
2020-10-29 12:02:12 -0400

    updates

--- routes/signal.js
+++ routes/signal.js
@@ -4,11 +4,19 @@ const Logger =               require('../services/logger')
 
 const logger = new Logger('auth')
 
+const getUniqueSignals = (arr, key) => {
+  return [...new Map(arr.map(item => [item[key], item])).values()]
+}
+
+let count = 0
+
 const signal = (router) => {
     router.use(async(req, res, next) => {
         count++
         console.log(`Signal number ${count} detected`)
-        console.log(req.body)
+        let uniqueSignals = getUniqueSignals(req.body, 'ibeaconUuid')
+        //console.log(req.body)
+        console.log(uniqueSignals)
 
         res.end()
   })
