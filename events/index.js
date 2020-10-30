5f5e9d5f08cc45a38b7b38da4e977a707134c15e
pdhoward <patrick.howard@hotmail.com>
2020-10-30 11:38:54 -0400

    integrate redis functions

--- events/index.js
+++ events/index.js
@@ -1,15 +1,14 @@
 
-const conn =                    require('../db')
 const {redisevents} =           require('./redis')
 
 ////////////////////////////////////////////////////////////////
-////////////////  Register Events and Connect    //////////////
+////////////////Register REDIS Events and Connect//////////////
 //////////////////////////////////////////////////////////////
 
 const events = () => {
   return new Promise(async (resolve, reject) => {
     let {pub, redis} = await redisevents()
-    resolve({db, pub, redis}) 
+    resolve({pub, redis}) 
   })  
 }
 
